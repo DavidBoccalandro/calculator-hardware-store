183525b97f32ea2f4d2b44bc9c3f639872cf3906
DavidBoccalandro <davidboccalandro@hotmail.com>
2022-12-11 18:59:45 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'calculator';
+  title = 'CALCULADORA FERRETERA';
+  finalPrice = 0;
+  finalWeight = 0;
+  finalFirstUnit = '';
+  finalSecondUnit = '';
+
+  priceCalculator = new FormGroup({
+    firstUnit: new FormControl(''),
+    firstBaseWeight: new FormControl('', Validators.required),
+    firstBasePrice: new FormControl('', Validators.required),
+    firstCurrentWeight: new FormControl('', Validators.required),
+  });
+
+  weightCalculator = new FormGroup({
+    secondUnit: new FormControl(''),
+    secondBaseWeight: new FormControl('', Validators.required),
+    secondBasePrice: new FormControl('', Validators.required),
+    secondCurrentPrice: new FormControl('', Validators.required),
+  });
+
+  calculatePrice() {
+    const baseWeight = parseFloat(
+      this.priceCalculator.value.firstBaseWeight as string
+    );
+    const basePrice = parseFloat(
+      this.priceCalculator.value.firstBasePrice as string
+    );
+    const currentWeight = parseFloat(
+      this.priceCalculator.value.firstCurrentWeight as string
+    );
+
+    this.finalFirstUnit = this.priceCalculator.value.firstUnit as string;
+    this.finalPrice = (currentWeight * basePrice) / baseWeight;
+  }
+
+  calculateWeight() {
+    const baseWeight = parseFloat(
+      this.weightCalculator.value.secondBaseWeight as string
+    );
+    const basePrice = parseFloat(
+      this.weightCalculator.value.secondBasePrice as string
+    );
+    const currentPrice = parseFloat(
+      this.weightCalculator.value.secondCurrentPrice as string
+    );
+
+    this.finalSecondUnit = this.weightCalculator.value.secondUnit as string;
+    this.finalWeight = (currentPrice * baseWeight) / basePrice;
+  }
+
+  writeUnit() {
+    this.finalFirstUnit = this.priceCalculator.value.firstUnit as string;
+  }
 }
